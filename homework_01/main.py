e2f17ee135507d955a6066dfd7fb685c9b27eb8e
stinger <alavr.97@mail.ru>
2024-04-25 22:27:08 +0300

    replace repo

---
+++ homework_01/main.py
@@ -0,0 +1,29 @@
+"""
+Домашнее задание №1
+Функции и структуры данных
+"""
+
+def power_numbers(*nums):
+    return [x * x for x in nums]
+
+ODD = "odd"
+EVEN = "even"
+PRIME = "prime"
+
+def is_prime(prime: int) -> bool:
+    if prime > 1:
+        for q in range(2, int(prime ** 0.5) + 1):
+            if prime % q == 0:
+                return False
+        return True
+    else:
+        return False
+
+def filter_numbers(nums, filter_type):
+    if filter_type == ODD:
+        return list(filter(lambda q: (q % 2 == 1), nums))
+    if filter_type == EVEN:
+        return list(filter(lambda q: (q % 2 == 0), nums))
+    if filter_type == PRIME:
+        return list(filter(is_prime, nums))
+    raise ValueError(f"ERROR: {filter_type}")
